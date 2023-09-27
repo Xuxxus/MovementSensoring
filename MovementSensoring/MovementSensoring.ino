85f2e432a9d4569744247df87178afa9b18927cf
Joao-Alb <joao.lima.alb@gmail.com>
2023-09-27 17:01:56 -0300

    First try; not working

--- MovementSensoring/MovementSensoring.ino
+++ MovementSensoring/MovementSensoring.ino
@@ -165,17 +165,17 @@ void diselect_MPU(){
 
 
 void writeFile(fs::FS &fs, const char * path, const char * message){
-    Serial.printf("Writing file: %s\n", path);
+    //Serial.printf("Writing file: %s\n", path);
 
     File file = fs.open(path, FILE_WRITE);
     if(!file){
-        Serial.println("Failed to open file for writing");
+       // Serial.println("Failed to open file for writing");
         return;
     }
     if(file.print(message)){
-        Serial.println("File written");
+       // Serial.println("File written");
     } else {
-        Serial.println("Write failed");
+       // Serial.println("Write failed");
     }
     file.close();
 }
